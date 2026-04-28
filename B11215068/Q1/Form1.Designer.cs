8770be494a88feb786b6540412db580a44246460
cake11298 <cake11298@gmail.com>
2026-04-28 21:02:36 +0800

    with assignments

---
+++ B11215068/Q1/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Q1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.process1 = new System.Diagnostics.Process();
+            this.serviceController1 = new System.ServiceProcess.ServiceController();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1309, 29);
+            this.button1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 45);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "連線";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1017, 33);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(212, 36);
+            this.textBox3.TabIndex = 19;
+            this.textBox3.Text = "2025";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(874, 40);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 24);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "本機 Port";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(572, 33);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(212, 36);
+            this.textBox2.TabIndex = 17;
+            this.textBox2.Text = "2024";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(416, 40);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 24);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "目標 Port";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 33);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 36);
+            this.textBox1.TabIndex = 15;
+            this.textBox1.Text = "127.0.0.1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 40);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 24);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "目標 IP";
+            // 
+            // process1
+            // 
+            this.process1.StartInfo.Domain = "";
+            this.process1.StartInfo.LoadUserProfile = false;
+            this.process1.StartInfo.Password = null;
+            this.process1.StartInfo.StandardErrorEncoding = null;
+            this.process1.StartInfo.StandardOutputEncoding = null;
+            this.process1.StartInfo.UserName = "";
+            this.process1.SynchronizingObject = this;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(124, 98);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(77, 28);
+            this.radioButton1.TabIndex = 22;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Red";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(264, 98);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(83, 28);
+            this.radioButton2.TabIndex = 23;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Green";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(377, 98);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(96, 28);
+            this.radioButton3.TabIndex = 24;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Blue";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(551, 98);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(93, 28);
+            this.radioButton4.TabIndex = 25;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Black";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(878, 98);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(60, 36);
+            this.numericUpDown1.TabIndex = 26;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1491, 600);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Diagnostics.Process process1;
+        private System.ServiceProcess.ServiceController serviceController1;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ B11215068/Q1/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;         //匯入網路通訊協定相關參數
+using System.Net.Sockets; //匯入網路插座功能函數
+using System.Threading;
+using System.Net.NetworkInformation;   //匯入多執行緒功能函數
+
+namespace Q1
+{
+    public partial class Form1 : Form
+    {
+        UdpClient U; //宣告UDP通訊物件
+        //自行決定同步或非同步的方式
+        /*
+        private List<List<Point>> allLines;   // 儲存所有繪製的線條，每條線由一系列的點組成
+        private List<Point> currentLine;      // 當前正在繪製的線
+        */
+
+        private bool isDrawing;               // 標示是否正在繪製
+        Thread Th;
+        private List<Point> points;
+        private Color currentColor = Color.Blue;
+        private int currentThickness = 2;
+        private List<Stroke> strokes = new List<Stroke>();
+        private Stroke currentStroke;
+        public enum ToolType { Pen }
+        public class Stroke
+        {
+            public List<Point> Points { get; set; } = new List<Point>();
+            public Color StrokeColor { get; set; } = Color.Blue;
+            public int Thickness { get; set; } = 2;
+        }
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;          // 防止閃爍，開啟雙緩衝
+            points = new List<Point>();
+            //allLines = new List<List<Point>>();  // 初始化所有線條的列表
+            //currentLine = new List<Point>();     // 初始化當前線條的點列表
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //顯示本機IP於標題列
+            this.Text = "Q1 資工系_B11215068_楊智宇" + MyIP();
+            this.MouseDown += new MouseEventHandler(MainForm_MouseDown);
+            this.MouseMove += new MouseEventHandler(MainForm_MouseMove);
+            this.MouseUp += new MouseEventHandler(MainForm_MouseUp);
+            this.Paint += new PaintEventHandler(MainForm_Paint);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //自行決定同步或非同步的方式建立監聽
+            //建立經聽之後，讓按鍵失效，不能(也不需要)重複開啟監聽
+            Th = new Thread(Listen);
+            Th.IsBackground = true;
+            Th.Start();
+            button1.Enabled = false;
+        }
+        //監聽副程序(自行定義)
+        private void Listen()
+        {
+            int Port = int.Parse(textBox3.Text);
+            U = new UdpClient(Port);
+            IPEndPoint EP = new IPEndPoint(IPAddress.Any, Port);
+            while (true)
+            {
+                byte[] B = U.Receive(ref EP);
+                string data = Encoding.Default.GetString(B);
+                Stroke s = StringToStroke(data);
+                if (s != null)
+                {
+                    this.Invoke((Action)(() =>
+                    {
+                        strokes.Add(s);
+                        this.Invalidate();
+                    }));
+                }
+            }
+        }
+
+        //關閉監聽執行續(如果有的話)
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                //Th.Abort(); //關閉監聽執行緒
+                U?.Close();  //關閉監聽器
+            }
+            catch
+            {
+                //忽略錯誤，程式繼續執行
+            }
+        }
+
+        // 鼠標按下事件，開始記錄起始點
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                //currentLine.Clear();  // 開始新的一條線時清空當前線條
+                //currentLine.Add(e.Location);  // 記錄起始點
+                currentStroke = new Stroke { StrokeColor = currentColor, Thickness = currentThickness };
+                currentStroke.Points.Add(e.Location);
+                isDrawing = true;  // 開始繪製
+            }
+        }
+
+        // 鼠標移動事件，記錄經過的點並連接
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                currentStroke.Points.Add(e.Location);   
+                this.Invalidate();  // 重新繪製表單
+            }
+        }
+
+        // 鼠標放開事件，停止繪製
+        private void MainForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && currentStroke != null)
+            {
+                isDrawing = false;
+                strokes.Add(currentStroke);
+                string IP = textBox1.Text;
+                int Port = int.Parse(textBox2.Text);
+                string data = StrokeToString(currentStroke);
+                byte[] B = Encoding.Default.GetBytes(data);
+                UdpClient S = new UdpClient();
+                S.Send(B, B.Length, IP, Port);
+                S.Close();
+                this.Invalidate();
+            }
+        }
+
+        // Paint 事件，繪製經過的所有連線
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            foreach (var stroke in strokes)
+            {
+                using (var pen = new Pen(stroke.StrokeColor, stroke.Thickness))
+                {
+                    for (int i = 1; i < stroke.Points.Count; i++)
+                        g.DrawLine(pen, stroke.Points[i - 1], stroke.Points[i]);
+                }
+            }
+            if (currentStroke != null && currentStroke.Points.Count > 1)
+            {
+                using (var pen = new Pen(currentStroke.StrokeColor, currentStroke.Thickness))
+                {
+                    for (int i = 1; i < currentStroke.Points.Count; i++)
+                        g.DrawLine(pen, currentStroke.Points[i - 1], currentStroke.Points[i]);
+                }
+            }
+        }
+        // 自行決定是否採用序列與反序列化的函式
+        public static List<List<Point>> StringToList(string str)
+        {
+            var pointsList = new List<List<Point>>();
+            var listStrings = str.Split('|');
+
+            foreach (var listStr in listStrings)
+            {
+                var innerList = new List<Point>();
+                var pointStrings = listStr.Split(';');
+
+                foreach (var pointStr in pointStrings)
+                {
+                    if (!string.IsNullOrEmpty(pointStr))
+                    {
+                        var parts = pointStr.Split(',');
+                        var x = int.Parse(parts[0]);
+                        var y = int.Parse(parts[1]);
+                        innerList.Add(new Point(x, y));
+                    }
+                }
+
+                pointsList.Add(innerList);
+            }
+
+            return pointsList;
+        }
+
+
+        static string StrokeToString(Stroke s)
+        {
+            string colorPart = $"COLOR:{s.StrokeColor.R},{s.StrokeColor.G},{s.StrokeColor.B}";
+            string thickPart = $"THICK:{s.Thickness}";
+            var pts = new List<string>();
+            foreach (var p in s.Points)
+                pts.Add($"{p.X},{p.Y}");
+            string pointPart = "POINTS:" + string.Join(";", pts);
+            return colorPart + "|" + thickPart + "|" + pointPart;
+        }
+        static Stroke StringToStroke(string data)
+        {
+            try
+            {
+                var parts = data.Split('|');
+                var cRaw = parts[0].Replace("COLOR:", "").Split(',');
+                var tRaw = parts[1].Replace("THICK:", "");
+                var pRaw = parts[2].Replace("POINTS:", "").Split(';');
+                Color c = Color.FromArgb(int.Parse(cRaw[0]), int.Parse(cRaw[1]), int.Parse(cRaw[2]));
+                int thick = int.Parse(tRaw);
+                var stroke = new Stroke { StrokeColor = c, Thickness = thick };
+                foreach (var seg in pRaw)
+                {
+                    var xy = seg.Split(',');
+                    if (xy.Length == 2)
+                        stroke.Points.Add(new Point(int.Parse(xy[0]), int.Parse(xy[1])));
+                }
+                return stroke;
+            }
+            catch { return null; }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked) currentColor = Color.Red;
+        }
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked) currentColor = Color.Green;
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked) currentColor = Color.Blue;
+        }
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton4.Checked) currentColor = Color.Black;
+        }
+        private void numericUpDown1_CheckedChanged(object sender, EventArgs e)
+        {
+            currentThickness = (int)numericUpDown1.Value;
+        }
+        public string MyIP() { return " 140.118.126.38"; }
+    }
+}
